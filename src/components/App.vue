11a1b035ae78996dbd0af9859a7884285b8285c3
Erichain <zhengyuchuan@meituan.com>
2018-02-26 11:55:32 +0800

    Add initial config and page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,9 @@
 <template>
-  
+  <h1>This is a demo app used to test critical plugin.</h1>
 </template>
 
 <script>
   export default {
-    name: 'app',
+    name: 'App',
   }
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './components/App';
+
+new Vue({
+  components: { App },
+}).$mount('#app');
